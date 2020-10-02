546776d563e6a0bf0fd891c86f197e830d1d4003
Amista <62178839+amistaa@users.noreply.github.com>
2020-10-03 06:53:30 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
